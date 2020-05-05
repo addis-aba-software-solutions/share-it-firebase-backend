becbd5bd3f019fffa98f53ed854fa2f4cab2d2f1
kidusss <kidussolomon2004@gmail.com>
2020-05-05 09:12:21 +0300

    server entry point created

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,12 @@
 const functions = require('firebase-functions');
+const app = require('express')();
+const firebase = require('firebase');
+const { firebaseConfig } = require('./utils/firebase');
+firebase.initializeApp(firebaseConfig);
+const { validate } = require('./utils/validater');
+// const { API } = require('./utils/api');
+const { login, signup } = require('./Accounts/users');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+app.post('/login', validate('login'), login);
+app.post('/signup', validate('signup'), signup);
+exports.api = functions.https.onRequest(app);
