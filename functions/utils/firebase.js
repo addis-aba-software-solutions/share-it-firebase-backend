2df0c1924241041ecd5d80c32879d7b228d932e1
kidusss <kidussolomon2004@gmail.com>
2020-05-05 09:11:55 +0300

    firebase common config

---
+++ functions/utils/firebase.js
@@ -0,0 +1,15 @@
+const admin = require('firebase-admin');
+admin.initializeApp();
+const db = admin.firestore();
+// TODO Change to share-it app config
+const firebaseConfig = {
+  apiKey: 'AIzaSyDt2YfICPZr7gFzPK25SzwkSPM0JinwAQY',
+  authDomain: 'share-it-5c4ec.firebaseapp.com',
+  databaseURL: 'https://share-it-5c4ec.firebaseio.com',
+  projectId: 'share-it-5c4ec',
+  storageBucket: 'share-it-5c4ec.appspot.com',
+  messagingSenderId: '719844398617',
+  appId: '1:719844398617:web:2d3efb4f29a7c10f2b3c88',
+  measurementId: 'G-6MD6G5DXKS',
+};
+module.exports = { admin, db, firebaseConfig };
